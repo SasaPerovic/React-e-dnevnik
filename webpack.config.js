96d97a8bd6002898fffa2449aa9c83521ebd1f7c
SasaPerovic <sasans87@gmail.com>
2019-03-26 00:35:57 +0100

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.js'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader",   // translates CSS into CommonJS
+          "sass-loader"   // compiles Sass to CSS
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|ico|svg)$/,
+        use: 'file-loader?name=assets/images/[name].[hash].[ext]',
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    modules: [
+      'node_modules',
+      path.resolve(__dirname, 'src'),
+    ]
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'ENV': JSON.stringify(process.env.ENV || 'development')
+      }
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './dist',
+    hot: true
+  }
+};
