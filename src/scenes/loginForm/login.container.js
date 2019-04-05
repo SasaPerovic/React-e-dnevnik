45bc600c042cd24989aa204fb8c207de82dbac9e
SasaPerovic <sasans87@gmail.com>
2019-04-05 23:55:52 +0200

    Update

--- src/scenes/loginForm/login.container.js
+++ src/scenes/loginForm/login.container.js
@@ -4,13 +4,6 @@ import { loginUser } from '@/store/users/action'
 
 import login from './login'
 
-// const mapStateToProps = (state) => {
-//   const { search11 } = state.LD
-//   return {
-//     search11,
-//   }
-// }
-
 const matchDispatchToProps = {
   loginUser,
 }
